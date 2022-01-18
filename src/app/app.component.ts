6be6e1607e606581d5a71b2a58ba70f625753618
Saurav <saurav123patel@gmail.com>
2022-01-18 11:12:27 +0530

    3 column carsouel design complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,24 @@ import { Title } from '@angular/platform-browser';
 export class AppComponent {
   title = 'Angular Components';
   //itTypeVideo=false
-  carouselData=[
+  carouselData = [
     {
-      src:'../assets/img/img1.png',
-      title:'Title',
-      type:'img',
-      decs:'With supporting text below as a natural lead-in to additional content.'
+      src: 'https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjMyMDc0fQ&s=7c625ea379640da3ef2e24f20df7ce8d',
+      title: 'Title',
+      type: 'img',
+      decs: 'With supporting text below as a natural lead-in to additional content.'
     },
     {
-      src:'../assets/img/img1.png',
-      title:'Title',
-      type:'img',
-      decs:'With supporting text below as a natural lead-in to additional content.'
+      src: '../assets/video/Tropical.mp4',
+      title: 'Title',
+      type: 'video',
+      decs: 'With supporting text below as a natural lead-in to additional content.'
     },
     {
-      src:'../assets/img/img1.png',
-      title:'Title',
-      type:'img',
-      decs:'With supporting text below as a natural lead-in to additional content.'
+      src: 'https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&fit=max&ixid=eyJhcHBfaWQiOjMyMDc0fQ&s=7c625ea379640da3ef2e24f20df7ce8d',
+      title: 'Title',
+      type: 'img',
+      decs: 'With supporting text below as a natural lead-in to additional content.'
     }
   ];
 }
